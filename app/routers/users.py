6233673fc5a4aef862064e612bfbfa0b059f3071
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2026-08-29 13:18:37 +0200

    Upgrade to Python 3.13 and refresh deps

--- app/routers/users.py
+++ app/routers/users.py
@@ -223,7 +223,7 @@ def update_user(
 
     if not fields:
         raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
             detail="No fields to update",
         )
 
